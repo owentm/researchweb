a513bbffcf3a05aa33d6d6ddaf06ec2d98380bb0
Owen Murphy <owentmurp@gmail.com>
2024-03-01 09:27:24 -0600

    added firebase

---
+++ database.js
@@ -0,0 +1,49 @@
+<script type="module">
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDqc7aZUnBM6BM6ri0m2oj7FkNKnDESirM",
+
+    authDomain: "linkedspace-76d77.firebaseapp.com",
+
+    projectId: "linkedspace-76d77",
+
+    storageBucket: "linkedspace-76d77.appspot.com",
+
+    messagingSenderId: "897208957659",
+
+    appId: "1:897208957659:web:e129682558190395686afe",
+
+    measurementId: "G-NN1CQYQGG0"
+
+  };
+
+
+
+
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+  const analytics = getAnalytics(app);
+
+</script>
+
+
